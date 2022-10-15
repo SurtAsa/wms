40c5f1e8c5654b26175725c50f423db9ebaac128
Paulo Martins <paulo.martins@simbioseventures.com>
2022-10-15 20:53:50 -0300

    feat: add send selector

--- wms.py
+++ wms.py
@@ -12,14 +12,18 @@ data_dict = contatos.to_dict('list')
 
 leads = data_dict['wpp']
 messages = data_dict['msg']
-combo = zip(leads,messages)
+send = data_dict['send']
+value = data_dict['value']
+description = data_dict['description']
+pix = data_dict['pix']
+combo = zip(leads,messages,send, value, description, pix)
 first = True
-for lead,messages in combo:
+def sender(first,lead,messages):
     time.sleep(4)
     web.open("https://web.whatsapp.com/send?phone="+lead+"&text="+messages)
     if first:
         time.sleep(4)
-        first=False
+        first = False
     width,height = pg.size()
     pg.click(width/2,height/2)
     time.sleep(8)
@@ -28,3 +32,9 @@ for lead,messages in combo:
     pg.press('enter')
     time.sleep(5)
     pg.hotkey('ctrl','w')
+for lead,messages,send, value, description, pix in combo:
+    if send == "yes":
+        messages = (f"Boa noite irmão! Como está por ai? Estou mandando os valores que ficaram pendentes para o mês de Outubro/22. Se preferir pagar por PIX a minha chave é: {pix} Se optar pelo pagamento via PIX, peço apenas que me mande o comprovante pelo privado. Valor: {value}. Descrição: {description}. Qualquer dúvida estou à disposição! V.S.S.V.")
+        sender(first,lead,messages)
+    elif send == "admin":
+        sender(first,lead,messages)
